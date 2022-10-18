7bc094a7b3cf3b901f8a27b8df6f8a5f50dcd898
Nana <prajna270505@gmail.com>
2022-10-18 11:31:03 +0700

    endpoint

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,35 @@ Route::get('/', function () {
 Route::get('/saya', function () {
     return "hallo";
 });
+
+// string
+Route::get('/saya', function () {
+    return "hallo";
+});
+
+// array
+Route::get('/coba1', function () {
+    return ['nana', 'nana', 'nana'];
+});
+
+// objek json
+Route::get('/coba2', function () {
+    return [
+        'Nama' => 'Nana',
+        'Kelas' => '12 RPL 5',
+        'NIS' => 3103120176
+    ];
+});
+
+// objek json
+Route::get('/coba3', function () {
+    return response()->json(
+        [
+            'Nama' => 'Prajna',
+            'Kelas' => '12 RPL 5',
+            'NIS' => 3103120176
+        ],201
+    );
+});
+
+
